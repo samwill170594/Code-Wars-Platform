b471c31d7a643e6da840195545e434ddf520f1a4
ASSAMOI Fred Williams <assamoifredwilliams@gmail.com>
2023-08-03 11:18:24 +0000

    PigLatin.py

---
+++ PYTHON/PigLatin.py
@@ -0,0 +1,17 @@
+# challenge link : https://www.codewars.com/kata/520b9d2ad5c005041100000f/train/python
+
+"""
+Move the first letter of each word to the end of it, 
+then add "ay" to the end of the word. Leave punctuation marks untouched.
+"""
+
+import string
+
+def pig_it(text):
+    words = text.split(" ")
+    word_to_add = "ay"
+    return " ".join([word[1:] + word[0] + word_to_add if word not in string.punctuation else word for word in words])
+
+
+
+print(pig_it("Hello world !"))
